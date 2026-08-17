5b9b6b1e9948e4b7f9aebdbdf3f01b69fef6df22
Francesco Lorenzo D'Amico <francescodamico448@gmail.com>
2026-08-17 18:45:20 +0200

    Add Page for Application with own model

---
+++ RazorPagesMovie/Pages/Bewerbung.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorPagesMovie.Pages
+{
+    public class BewerbungModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
