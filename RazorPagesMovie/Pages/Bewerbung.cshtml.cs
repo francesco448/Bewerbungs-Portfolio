ee9e8de35444ef476ee94d62f51504dfa49a89b5
Francesco Lorenzo D'Amico <francescodamico448@gmail.com>
2026-08-18 16:44:42 +0200

    Contactform saves input into db

--- RazorPagesMovie/Pages/Bewerbung.cshtml.cs
+++ RazorPagesMovie/Pages/Bewerbung.cshtml.cs
@@ -1,12 +1,62 @@
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorPagesMovie.Data;
+using RazorPagesMovie.Models;
 
 namespace RazorPagesMovie.Pages
 {
     public class BewerbungModel : PageModel
     {
+        private readonly RazorPagesMovieContext _context;
+
+        public BewerbungModel(RazorPagesMovieContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public string Name { get; set; } = string.Empty;
+
+        [BindProperty]
+        public string Email { get; set; } = string.Empty;
+
+        [BindProperty]
+        public string? Phone { get; set; }
+
+        [BindProperty]
+        public string? Subject { get; set; }
+
+        [BindProperty]
+        public string Message { get; set; } = string.Empty;
+
+        public bool ContactFormSubmitted { get; set; }
+
         public void OnGet()
         {
 
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            var contactMessage = new ContactMessage
+            {
+                Name = Name,
+                Email = Email,
+                Phone = Phone,
+                Subject = Subject,
+                Message = Message
+            };
+
+            if (!TryValidateModel(contactMessage, nameof(ContactMessage)))
+            {
+                return Page();
+            }
+
+            _context.ContactMessage.Add(contactMessage);
+            await _context.SaveChangesAsync();
+
+            ContactFormSubmitted = true;
+            return Page();
         }
     }
 }
