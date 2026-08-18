ee43dc60d86b1d1b1dcd8659751b4856fbba78ab
Francesco Lorenzo D'Amico <francescodamico448@gmail.com>
2026-08-18 16:46:57 +0200

    Create EF-Migration for NotificationSent-field

---
+++ RazorPagesMovie/Migrations/20260818134749_AddNotificationSentToContactMessage.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RazorPagesMovie.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddNotificationSentToContactMessage : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Message",
+                table: "ContactMessage",
+                type: "nvarchar(max)",
+                maxLength: 5000,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(4000)",
+                oldMaxLength: 4000);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "NotificationSent",
+                table: "ContactMessage",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "NotificationSent",
+                table: "ContactMessage");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Message",
+                table: "ContactMessage",
+                type: "nvarchar(4000)",
+                maxLength: 4000,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldMaxLength: 5000);
+        }
+    }
+}
