be55d56897209a72f06157275ff0ebc5fb0b6d96
Francesco Lorenzo D'Amico <francescodamico448@gmail.com>
2026-08-26 15:13:58 +0200

    Remove Movie feature and drop Movie table via migration

--- RazorPagesMovie/Data/RazorPagesMovieContext.cs
+++ RazorPagesMovie/Data/RazorPagesMovieContext.cs
@@ -4,7 +4,6 @@ namespace RazorPagesMovie.Data;
 
 public class RazorPagesMovieContext(DbContextOptions<RazorPagesMovieContext> options) : DbContext(options)
 {
-    public DbSet<RazorPagesMovie.Models.Movie> Movie { get; set; } = default!;
     public DbSet<RazorPagesMovie.Models.AdminUser> AdminUser { get; set; } = default!;
     public DbSet<RazorPagesMovie.Models.ContactMessage> ContactMessage { get; set; } = default!;
 }
