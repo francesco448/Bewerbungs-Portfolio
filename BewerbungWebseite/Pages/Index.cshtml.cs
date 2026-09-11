bac82b156822775410c0c63a43648aa90ff72f2e
Francesco Lorenzo D'Amico <francescodamico448@gmail.com>
2026-09-11 13:41:45 +0200

    Render the public homepage from the editable section partials

--- BewerbungWebseite/Pages/Index.cshtml.cs
+++ BewerbungWebseite/Pages/Index.cshtml.cs
@@ -1,12 +1,20 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using BewerbungsSeite.Models.SiteContent;
+using BewerbungsSeite.Services;
 
 namespace BewerbungsSeite.Pages
 {
-    public class IndexModel : PageModel
+    public class IndexModel(IPageContentService pageContentService) : PageModel
     {
-        public void OnGet()
-        {
+        /// <summary>Der aus der Datenbank geladene Seiteninhalt.</summary>
+        public PageContent PageContent { get; private set; } = new();
 
+        /// <summary>Auf der oeffentlichen Seite nie aktiv; nur die Admin-Seite rendert im Editiermodus.</summary>
+        public bool EditMode => false;
+
+        public async Task OnGetAsync()
+        {
+            PageContent = await pageContentService.GetAsync(HttpContext.RequestAborted);
         }
     }
 }
